8e5b6addebccb927c7b04bf00e8194fc7b12370c
David <1426621703@qq.com>
2022-06-09 19:08:35 +0800

    first-commit-01

---
+++ src/test/java/com/hust/dao/TestDemo.java
@@ -0,0 +1,12 @@
+package com.hust.dao;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestDemo {
+
+    @Test
+    public void test(){
+        System.out.println("gitTest");
+    }
+}
